4df371e9f6b5b0cf10564496ee248f929432c0ae
Geir <geir.drange@gmail.com>
2023-02-07 20:08:10 +0100

    Fix GHDL script

--- script/ghdl_run_all.sh
+++ script/ghdl_run_all.sh
@@ -78,4 +78,4 @@ ghdl -a --std=08 -frelaxed-rules --work=nw_codec ../nw_codec/src/nw_sl_codec_pkg
 ghdl -a --std=08 -frelaxed-rules --work=nw_codec ../nw_codec/src/nw_codec_context.vhd
 ghdl -a --std=08 -frelaxed-rules --work=work ../nw_codec/tb/nw_codec_tb.vhd
 ghdl -e --std=08 -frelaxed-rules --work=work nw_codec_tb
-ghdl -r --std=08 -frelaxed-rules --work=work nw_codec_tb -gGC_GHDL=1
+ghdl -r --std=08 -frelaxed-rules --work=work nw_codec_tb
